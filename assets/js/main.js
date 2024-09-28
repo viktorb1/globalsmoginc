0fd10bf872e52239b03746300d98606c3d1ba6d2
Viktor Babchanik <vbabchanik@ucdavis.edu>
2024-09-28 12:20:07 -0700

    Fix scrolling bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,7 +64,7 @@
   let selectHeader = select('#header')
   if (selectHeader) {
     const headerScrolled = () => {
-      if (window.scrollY > 100) {
+      if (window.scrollY > 20) {
         selectHeader.classList.add('header-scrolled')
       } else {
         selectHeader.classList.remove('header-scrolled')
